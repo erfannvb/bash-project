a8bf8ef557af3d06cbdda6da19ae9d4e0ae94577
Erfan Navab <erfannavab@gmail.com>
2023-10-10 23:28:35 +0330

    - add second project

---
+++ src/main/java/nvb/dev/SecondProject/List.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+while [ true ]; do
+  select option in create print replace
+  do
+    case $option in
+
+      "create")
+        for i in {1..5}; do
+          echo >> "File${i}.txt"
+        done
+        ;;
+
+      "print")
+        for i in {1..5}; do
+          echo "Hello World" >> "File${i}.txt"
+        done
+        ;;
+
+      "replace")
+        for i in {1..5}; do
+          sed 's/World/Bash/g' File${i}.txt | cat > File${i}.txt
+        done
+        ;;
+
+    esac
+  done
+done

fb0f53a9ea3534cc2f2e902de63e1c749fca0b5c
Erfan Navab <erfannavab@gmail.com>
2023-10-10 22:26:05 +0330

    - implement Add Task - implement Add to Completed Task - implement Show Unfinished Tasks

---
+++ src/main/java/nvb/dev/TO-DO.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+while [ true ]; do
+
+  echo "-------------------"
+  echo "1. Add Task"
+  echo "2. Add to Completed Tasks"
+  echo "3. Show Unfinished Tasks"
+  echo "-------------------"
+
+  echo "Choose an option"
+  read choose
+
+  case $choose in
+
+  1)
+    echo "Enter a task: "
+    read addTask
+    if [ -f taskList.txt ]; then
+      echo "$addTask" >> taskList.txt
+    else
+      touch taskList.txt
+      echo "$addTask" >> taskList.txt
+    fi
+    ;;
+
+  2)
+    echo "Enter the task that is completed: "
+    read comTask
+    if grep -Fxq "$comTask" taskList.txt; then
+      echo "$comTask" >> completedTask.txt
+      echo "$comTask" >> removedTask.txt
+      sed -i "/$comTask/d" taskList.txt
+    else
+      echo "Not Found in taskList.txt"
+    fi
+    ;;
+
+  3)
+    if [[ -n $(cat taskList.txt) ]]; then
+      cat taskList.txt
+    else
+      echo "file is empty!"
+    fi
+    ;;
+
+  esac
+done
+
